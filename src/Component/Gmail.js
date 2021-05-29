ff232a7fde73e5465c93f51d06fa8cc06b0ef679
ShahabAthar25 <shahabathar25@gmail.com>
2021-05-29 12:53:35 +0500

    Deployed The Site

--- src/Component/Gmail.js
+++ src/Component/Gmail.js
@@ -13,6 +13,7 @@ function Gmail({ auth, db }) {
 
     const [showCompose, setShowCompose] = useState(false)
     const [emailDetail, setEmailDetail] = useState(true)
+    const [starred, setStarred] = useState(true)
     const [id, setId] = useState("")
 
     return (
